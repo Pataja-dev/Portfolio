9aa3fbbcccbf29a54fbe5b735ca9d2b3cc449202
JL <johnlloydglee26@gmail.com>
2025-02-10 16:57:30 +0800

    in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import Navbar from "@/ui/app/navbar";
 export default function Home() {
@@ -5,30 +6,59 @@ export default function Home() {
     
     <div>
     {/* Sticky Navbar */}
-    {/* <div className="bg-stone-300 shadow-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 py-4">
-        <div className="flex justify-between items-center">
-          <div className="text-xl font-bold">
-            My Portfolio
-          </div>
-          <div className="space-x-4">
-            <a href="#about" className="text-gray-700 hover:text-blue-500">Home</a>
-            <a href="#about" className="text-gray-700 hover:text-blue-500">About</a>
-            <a href="#projects" className="text-gray-700 hover:text-blue-500">Projects</a>
-            <a href="#contact" className="text-gray-700 hover:text-blue-500">Contact</a>
-          </div>
-        </div>
-      </div>
-      
-
-<Navbar/>
-
+      <Navbar/>
+  
     {/* Main Content */}
     <div className="max-w-7xl mx-auto px-4 py-8">
-      <section id="about" className="py-20">
-        <h2 className="text-3xl font-bold">About Me</h2>
-        <p className="mt-4">This is a brief introduction about myself.</p>
-      </section>
+
+    {/* Home Content */}
+    <section id="home" className="py-10 flex flex-col md:flex-row items-center">
+          {/* Image Column */}
+          <div className="md:w-1/2">
+            <Image
+              src="/me.png" 
+              alt="John Lloyd Glee"
+              width={550} 
+              height={650} 
+              className="rounded-lg" 
+            />
+          </div>
+          {/* Text Column */}
+          <div className="md:w-1/2 md:pl-7 mt-8 md:mt-0 p-40  bg-stone-200  rounded-lg flex flex-col justify-center h-full">
+            <h1 className="text-7xl font-bold text-center">John Lloyd Glee</h1>
+            <p className="mt-4 text-center ">Tito Solutions Intern Software Developer</p>
+            <div className="mt-4 h-0.5 bg-gray-950 w-full"></div>
+            <div className="flex justify-center">
+
+          <button className="mt-8 bg-black text-white font-bold py-2 px-4 w-[100px] justify-center border-0 hover:bg-gray-800 transition duration-300">Projects</button>
+            </div>
+          </div>
+        </section>
+
+    {/* Skills Content */}
+    <section id="home" className="py-5 flex flex-col md:flex-row items-center">
+          
+          {/* Text Column */}
+          <div className="md:w-1/2 md:pl-8 mt-8 md:mt-0  p-6 rounded-lg flex flex-col justify-center h-full">
+          <h2 className="text-3xl font-bold">Skills</h2>
+        <p className="mt-4">Full-stack web developer proficient in Java and Python, delivering results-driven solutions. Expertise includes front-end technologies (HTML, CSS, Next.js, React Native) and back-end frameworks (Angular, Laravel). I contribute to projects from conception to deployment, leveraging SDLC Agile Scrum methodologies for effective project management and iterative progress. My strong communication and time management skills enable me to thrive in fast-paced environments, consistently delivering high-quality work within deadlines. At Tito Solutions, I collaborate effectively within development teams to understand and implement project requirements, ensuring transparency, adaptability, and successful outcomes.</p>
+        <h2 className="text-3xl font-bold">About Portfolio</h2>
+        <p className="mt-4">This portfolio showcases a collection of my most impactful projects and highlights my skills and experience in Web development. Here you'll find examples of my work. I'm passionate about creating user-friendly interfaces, crafting compelling narratives, developing innovative solutions, and I strive to bring that passion to every project I undertake. I'm constantly learning and evolving my skillset, and this portfolio reflects that growth. I hope you enjoy exploring my work and gain a better understanding of what I can offer.</p>
+      
+          </div>
+          {/* Image Column */}
+          <div className="md:w-1/2">
+            <Image
+              src="/skill.jpg" 
+              alt="John Lloyd Glee"
+              width={500} 
+              height={600} 
+              className="rounded-lg" 
+            />
+          </div>
+        </section>
+      <section id="skills" className="py-20">
+       </section>
 
       <section id="projects" className="py-20">
         <h2 className="text-3xl font-bold">Projects</h2>
--- src/ui/app/navbar.tsx
+++ src/ui/app/navbar.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image';
 const Navbar = () => {
   return (
     <nav className="bg-stone-300 shadow-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 py-4">
+      <div className=" mx-auto px-4 py-4">
         <div className="flex justify-between items-center">
           <div className="text-xl font-bold">
-          {/* <Link href="/">
+          <Link href="/">
                 <Image
-                  src="@/" // Path to your logo image in the public directory
-                  alt="My Logo"
-                  width={150} // Set the width of the image
-                  height={50} // Set the height of the image
-                  className="cursor-pointer" // Optional: Add cursor pointer for better UX
+                  src="/logo.png" 
+                  alt="Logo"
+                  width={150} 
+                  height={50} 
+                  className="cursor-pointer justify-self-start" 
                 />
-              </Link> */}
+              </Link>
           </div>
           <div className="space-x-4">
             <Link href="/" className="text-gray-700 hover:text-blue-500">Home</Link>
