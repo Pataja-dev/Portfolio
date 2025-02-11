c74eb56fc253d7a57874a69ff44b3f60539502a2
JL <johnlloydglee26@gmail.com>
2025-02-11 14:22:53 +0800

    Updated

--- src/ui/app/navbar.tsx
+++ src/ui/app/navbar.tsx
@@ -3,29 +3,30 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <nav className="bg-stone-300 shadow-md sticky top-0 z-50">
-      <div className=" mx-auto px-4 py-4">
-        <div className="flex justify-between items-center">
-          <div className="text-xl font-bold">
-          <Link href="/">
-                <Image
-                  src="/logo.png" 
-                  alt="Logo"
-                  width={150} 
-                  height={50} 
-                  className="cursor-pointer justify-self-start" 
-                />
-              </Link>
-          </div>
-          <div className="space-x-4">
-            <Link href="/" className="text-gray-700 hover:text-blue-500">Home</Link>
-            <Link href="/Skills" className="text-gray-700 hover:text-blue-500">Skills</Link>
-            <Link href="/projects" className="text-gray-700 hover:text-blue-500">Projects</Link>
-            <Link href="/contact" className="text-gray-700 hover:text-blue-500">Contact</Link>
-          </div>
+    <nav className="bg-stone-400  sticky top-0 start-0 z-50">
+    <div className=" mx-auto px-4 py-4">
+      <div className="flex justify-between items-center ">
+        <div className="text-xl font-bold">
+        <Link href="/">
+              <Image
+                src="/logo.png" 
+                alt="Logo"
+                width={150} 
+                height={50} 
+                className="cursor-pointer justify-self-start" 
+              />
+            </Link>
+        </div>
+        <div className="space-x-10 pr-20">
+          <Link href="/" className=" text-stone-900 hover:text-stone-500">Home</Link>
+          <Link href="#skills" className=" text-stone-900 hover:text-stone-500">Skills</Link>
+          <Link href="#projects" className=" text-stone-900 hover:text-stone-500">Projects</Link>
+          <Link href="#contact" className=" text-stone-900 hover:text-stone-500">Contact</Link>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
+
   );
 };
 
